e0dbca2e2e8348339d1ecd8be7726a1d919f7ede
techn0z <minhtrietamoled@gmail.com>
2024-11-20 17:34:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,38 +13,6 @@ app.get("/", (req, res) => {
 	res.sendFile(frontend("index.html"));
 });
 
-const storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, "uploads/");
-	},
-	filename: (req, file, cb) => {
-		const filePath = path.join("uploads", file.originalname);
-
-		if (fs.existsSync(filePath)) {
-			fs.unlinkSync(filePath);
-		}
-
-		cb(null, file.originalname);
-	},
-});
-
-const upload = multer({ storage });
-
-app.post("/upload", upload.single("file"), (req, res) => {
-	if (!req.file) {
-		return res.status(400).send("No file uploaded.");
-	}
-
-	if (!req.pass != process.env.pass) {
-		return res.status(400).send("Wrong password");
-	}
-
-	const filePath = `/uploads/${req.file.filename}`;
-	res.send({ filePath });
-});
-
-app.use("/uploads", express.static("uploads"));
-
 app.use(express.static("frontend"));
 
 const PORT = 3000 || process.env.PORT;
